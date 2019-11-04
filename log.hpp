5659c7cbe52585a67a55b635f4e12f3aadaabf78
agctu <agctu46@outlook.com>
2019-11-05 00:40:10 +0800

    add a switch to control output,I don't have a better method

--- log.hpp
+++ log.hpp
@@ -8,7 +8,7 @@ using namespace std;
 enum State { label, text ,lend};
 	
 class logger {
-
+	bool isopen = true;
 	ofstream fo;
 	ostream&  o;
 	logger(const logger& a):o(a.o) {}
@@ -22,12 +22,15 @@ public:
 	template <typename t>
 	void log(t s)
 	{
+		if (!isopen)
+			return;
 		o << s ;
 	}
 	template <typename t>
 	logger& operator<<(t s)
 	{
-		
+		if (!isopen)
+			return *this;
 		o << s;
 		return *this;
 	}
@@ -35,6 +38,8 @@ public:
 	logger& operator<<(ostream& (*pfn)(ostream&))
 	{
 		//used for endl
+		if (!isopen)
+			return *this;
 		o << pfn;
 		if (pfn == &endl<char,char_traits<char>>)
 			logindent();
@@ -43,11 +48,15 @@ public:
 	template<>
 	logger& operator<<(const char *s)
 	{
+		if (!isopen)
+			return *this;
 		return operator<<(string(s));
 	}
 	template<>
 	logger& operator<<(string s)
 	{
+		if (!isopen)
+			return *this;
 		if (state == label) {
 			logl(s);
 			state = text;
@@ -59,6 +68,8 @@ public:
 	}
 	logger& operator<<(State s)
 	{
+		if (!isopen)
+			return *this;
 		switch (s) {
 		case lend:
 			--indenttime;
@@ -76,6 +87,8 @@ public:
 	}
 	void logl(string l)
 	{
+		if (!isopen)
+			return;
 		if (!labelstack.empty() && labelstack.back() == l) {
 			labelstack.pop_back();
 			--indenttime;
@@ -90,6 +103,14 @@ public:
 			labelstack.push_back(l);
 		}
 		
+	}
+	void close()
+	{
+		isopen = false;
+	}
+	void open()
+	{
+		isopen = true;
 	}
 	~logger()
 	{
